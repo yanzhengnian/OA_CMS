1c33f317d7efdd41dd8e11a230598df99aac7f0d
zhengnian <zhenglin360@163.com>
2016-11-23 15:05:53 +0800

    add node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,15 @@ var gulp = require('gulp'), //本地安装gulp所用到的地方
 gulp.task('testLess', function () {
     gulp.src('src/less/style.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
-        .pipe(gulp.dest('src/css')); //将会在src/css下生成style.css
+        .pipe(gulp.dest('dist/css')); //将会在src/css下生成style.css
 });
 //定义一个testImage任务（自定义任务名称）
 gulp.task('testImage', function () {
-    gulp.src('images/*.{png,jpg,gif,ico}') //该任务针对的文件
+    gulp.src('src/images/*.{png,jpg,gif,ico}') //该任务针对的文件
         .pipe(imagemin()) //该任务调用的模块
-        .pipe(gulp.dest('src/images')); //将会在src/css下生成图片
+        .pipe(gulp.dest('dist/images')); //将会在src/css下生成图片
 });
-gulp.task('default',['testLess', 'testImage']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+gulp.task('default',['testImage']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
  
 //gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
 //gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
