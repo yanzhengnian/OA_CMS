8638c29752fb3852820e04e70f5c5dd8e1fb5f11
zhengnian <zhenglin360@163.com>
2016-11-24 20:39:11 +0800

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'), //本地安装gulp所用到的地方
     less = require('gulp-less'),
     imagemin = require('gulp-imagemin');
  
-//2.定义一个testLess任务（自定义任务名称）
+//2.1定义一个testLess任务（自定义任务名称）
 gulp.task('testLess', function () {
     gulp.src('src/less/style.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
