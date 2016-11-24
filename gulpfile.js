755735cc16a3375b166f5bb471fa12f5595a100f
zhengnian <zhenglin360@163.com>
2016-11-24 19:12:16 +0800

    add users-table

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-//导入工具包 require('node_modules里对应模块')
+//1.导入工具包 require('node_modules里对应模块')
 var gulp = require('gulp'), //本地安装gulp所用到的地方
     less = require('gulp-less'),
     imagemin = require('gulp-imagemin');
  
-//定义一个testLess任务（自定义任务名称）
+//2.定义一个testLess任务（自定义任务名称）
 gulp.task('testLess', function () {
     gulp.src('src/less/style.less') //该任务针对的文件
         .pipe(less()) //该任务调用的模块
